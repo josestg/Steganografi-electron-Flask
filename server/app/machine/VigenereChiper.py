b5acd757a0becf26c04010a6f1ef65d6305e1b80
josesitanggang <alfredojosesitanggang@gmail.com>
2018-10-12 00:32:52 +0700

    v.1.0

--- server/app/machine/VigenereChiper.py
+++ server/app/machine/VigenereChiper.py
@@ -1,43 +1,42 @@
-
 class VigenereChiper():
 
-	def __init__(self,path):
-		print('VIGENERE CHIPER')
-		self.__path = path
-		self.__info = {'filename':path}
+	def __init__(self,content=''):
+		self.__content = content
 
-	def read_file(self):
-		file = open(self.__path,'r')
-		self.__content = file.read()
-		file.close()
+	def read(self,path):
+		try:
+			file = open(path,'r')
+			self.__content = file.read()
+			file.close()
+			return self.__content
+		except FileNotFoundError as e:
+			print(e)
+
+	@property
+	def content(self):
 		return self.__content
 
-	def get_content(self):
-		self.read_file()
-		return self.__content
-
-	def encrypt_text(self,content,key):
+	def encrypt(self,content,key):
 		self.__content = ''
 		i = 0
 		for c  in content:
 			self.__content += chr((ord(c)+ord(key[i%len(key)]))%265)
 			i+=1
-		self.save_file(self.__path)
-		return self.__content
 		
-	def decrypt_text(self,content,key):
+	def decrypt(self,content,key):
 		self.__content = ''
 		i = 0
 		for c  in content:
 			self.__content += chr(abs((ord(c)-ord(key[i%len(key)]))%265))
 			i+=1
-		self.save_file(self.__path)
-		return self.__content
 
-	def save_file(self,path):
-		file = open(path,'w+')
-		file.write(self.__content)
-		file.close()
+	def save(self,path):
+		try:
+			file = open(path,'w+')
+			file.write(self.__content)
+			file.close()
+		except FileNotFoundError as e:
+			print(e)
 
 	def unicode_table(self,x=65,y=91):
 		n = y-x
@@ -49,13 +48,3 @@ class VigenereChiper():
 			table.append(column)
 			column = []
 		return table
-
-
-def main():
-	vc = VigenereChiper("../uploads/pesan.txt")
-	content = vc.read_file()
-	print(vc.decrypt_text(content,'kunci'))
-
-
-if __name__ == "__main__":
-	main()
